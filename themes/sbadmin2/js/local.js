ef667888a8aee95ff40c8a85813566ddeb74e6fb
IjorTengab <m_roji28@yahoo.com>
2019-10-24 11:44:12 +0700

    Sedikit perbaikan tampilan.

--- themes/sbadmin2/js/local.js
+++ themes/sbadmin2/js/local.js
@@ -1,3 +1,3 @@
 (function ($) {
-    $('.nav-pills, .nav-tabs').tabdrop({text: '<i class="glyphicon glyphicon-menu-hamburger"></i>'});
+    $('.nav-pills, .nav-tabs').tabdrop({text: '<i class="fa fa-bars"></i>'});
 })(jQuery);
