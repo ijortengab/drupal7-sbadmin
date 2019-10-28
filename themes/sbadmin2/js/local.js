74631cac8c689ace57743460f9c0505498e44a2a
IjorTengab <m_roji28@yahoo.com>
2019-10-28 15:09:35 +0700

    Menu link bertipe MENU_NORMAL_ITEM yang memiliki child pada sidebar sekarang bisa di-click.

--- themes/sbadmin2/js/local.js
+++ themes/sbadmin2/js/local.js
@@ -1,3 +1,32 @@
 (function ($) {
-    $('.nav-pills, .nav-tabs').tabdrop({text: '<i class="fa fa-bars"></i>'});
+
+/**
+ * Mengganti icon bar bawaan font-awesome yang lebih elegan dibandingkan
+ * default (glyphicon).
+ */
+$('.nav-pills, .nav-tabs').tabdrop({text: '<i class="fa fa-bars"></i>'});
+
+/**
+ * Eksekusi function `$('#side-menu').metisMenu()` pada file `/vendor/startbootstrap-sb-admin-2/3.3.7+1/js/sb-admin-2.js`
+ * berarti menjalankan jQuery plugin metisMenu() versi `1.1.3` pada file `/vendor/metisMenu/1.1.3/metisMenu.min.js`.
+ *
+ * Plugin `metisMenu` tidak terdapat event yang memberitahukan bahwa eksekusi
+ * sudah selesai. Namun, eksekusi `metisMenu()` yang sudah selesai akan
+ * menambahkan jQuery.data() yang disimpan pada DOM (lihat code) dan ini bisa
+ * kita jadikan penanda sebagai pengganti event dengan melakukan pengecekan
+ * menggunakan javascript setInterval() per 0.1 detik.
+ */
+var checkFinishingMetisMenu = setInterval(function() {
+    var data = $("#side-menu").data();
+    if (data.metisMenu) {
+        clearInterval(checkFinishingMetisMenu);
+        var $element = data.metisMenu.element;
+        // Jika link item merupakan MENU_NORMAL_ITEM, maka event click yang
+        // di-set oleh `metisMenu()` perlu kita revoke agar user bisa
+        // menge-click tautan link tersebut. Lihat pula function PHP
+        // `sbadmin2_menu::modified_menu_tree()`.
+        $element.find("li.menu-normal-item").has("ul").children("a").off("click.metisMenu");
+        $element.find("li.menu-normal-item").has("ul").children("ul").collapse("show");
+    }
+}, 100);
 })(jQuery);
